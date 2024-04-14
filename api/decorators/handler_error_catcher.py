7b14a57fc783b85b9301016657eddbc2d2e787aa
Nikita <twityxp@mail.ru>
2024-04-14 13:05:15 +0200

    - fix

--- api/decorators/handler_error_catcher.py
+++ api/decorators/handler_error_catcher.py
@@ -15,7 +15,20 @@ def handler_error_catcher(debug: bool = get_debug_mode()):
 
         def wrapper(*args, **kwargs):
             message = args[0]
-            user = get_or_create_user(message)
+
+            try:
+                user = get_or_create_user(message)
+            except Exception as e:
+
+                if debug:
+                    print(e)
+
+                user_id = message.from_user.id
+                language = get_default_language(message)
+                keyboard = get_menu_keyboard(language)
+                message_text = REQUEST_ERROR_MESSAGE[language]
+
+                return bot.send_message(user_id, message_text, reply_markup=keyboard)
 
             try:
                 if message.text and message.text in MENU_BUTTON_TEXT.values():
